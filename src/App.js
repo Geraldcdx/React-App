090738b34c544f297912bd010dc0d5ac40be44f7
Geraldcdx <geraldcdx5@gmail.com>
2023-04-23 10:38:43 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is just to test whether i can deploy on aws
         </p>
         <a
           className="App-link"
